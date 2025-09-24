5bd0d8ffeaff8f862dfa110a256d2fd24970748a
Pedro Carvalho <100386092+pedrocarvalhoit@users.noreply.github.com>
2025-09-24 16:08:02 +0100

    Strength Test Created

---
+++ pts-api-strength-test/src/main/java/com/carvalho/pts_api_strength_test/PtsApiStrengthTestApplication.java
@@ -0,0 +1,13 @@
+package com.carvalho.pts_api_strength_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtsApiStrengthTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PtsApiStrengthTestApplication.class, args);
+	}
+
+}
---
+++ pts-api-strength-test/src/test/java/com/carvalho/pts_api_strength_test/PtsApiStrengthTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carvalho.pts_api_strength_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtsApiStrengthTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
